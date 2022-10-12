474f9be33cde8bd9563387709c1999894bf04212
shailesh chaurasiya <shaileshc535@gmail.com>
2022-10-12 19:26:01 +0530

    bosst

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 6000);
+    }, 1000);
     return () => clearTimeout(timer);
   }, []);
 
